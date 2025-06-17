302485024f23f8c7f185251c4ae90db643400524
jhanley-doobay <jhanley@doobay.org>
2025-06-17 19:35:38 -0400

    Add basic outline

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Hello, World!");
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+// Initializes your app with credentials
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode:true, // enable to use socket mode
+  appToken: process.env.APP_TOKEN
+});
+
+(async () => {
+  const port = 3000
+  await app.start(process.env.PORT || port);
+  console.log('Bolt app started!!');
+})();
